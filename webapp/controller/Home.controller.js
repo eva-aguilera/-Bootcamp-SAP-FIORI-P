e2c09e1d659120b682ecf251d12b1235d291ce73
Eva Aguilera <eva2aguilera0tauda01@gmail.com>
2025-03-23 15:58:45 +0000

    combobox y filtros

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "com/bootcamp/sapui5/freestylee/utils/HomeHelper"
-], (Controller, HomeHelper) => {
+    "com/bootcamp/sapui5/freestylee/utils/HomeHelper",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller, HomeHelper, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("com.bootcamp.sapui5.freestylee.controller.Home", {
@@ -9,12 +11,12 @@ sap.ui.define([
             this.oRouter = this.getOwnerComponent().getRouter();
         },
 
-        onPress: async function(){
-            let oDatos = await HomeHelper.getDataProducts(); 
+        onPress: async function () {
+            let oDatos = await HomeHelper.getDataProducts();
             await HomeHelper.setProductModel(this, oDatos[0].results);
         },
 
-        onItemPress: function(oEvent){
+        onItemPress: function (oEvent) {
             console.log("Evento onItemPress ejecutado");
             let oSource = oEvent.getSource();
 
@@ -24,6 +26,30 @@ sap.ui.define([
             this.oRouter.navTo("detail", {
                 ProductID: aDatos.ProductID
             });
+        },
+        //onChange: function (oEvent) {
+        //let oFilter= [];
+        //let oSource = oEvent.getSource();
+        //let oTable = this.getView().byId("idProductsTable");
+        //let oBinding =oTable.getBinding("items");
+
+        //if (oSource.getValue()) {
+        //oFilter = new Filter("ProductID", FilterOperator.EQ, oSource.getValue());
+
+        //}
+        //oBinding.filter(oFilter);
+
+    //},
+        onPress: async function () {
+        let oFilter = [];
+        let sValue = this.byId("inputID").getValue();
+
+        if (sValue) {
+            oFilter = new Filter("ProductID", FilterOperator.EQ, sValue)
         }
+
+        let oDatos = await HomeHelper.getDataProducts([oFilter]);
+        await HomeHelper.setProductModel(this, oDatos[0].results);
+    },
     });
 });
